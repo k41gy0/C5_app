ae4b1a2e20390eb03ebfe0c6c70552b4c8104839
k41gy0 <kayka1gy0@gmail.com>
2025-07-17 20:47:50 +0900

    -Initial commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    ua = request.headers.get('User-Agent', '')
+    print('UA: ', ua)
+    #if "ChatGPT" in ua:
+    #    return "Hello, ChatGPT!"
+    return "Hello, Scanner!"
+
+if __name__ == '__main__':
+    app.run(port=8000)
