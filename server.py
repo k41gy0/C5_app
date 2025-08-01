de1d0da7ba32576254f05b4ec5b70341508f10ec
k41gy0 <kayka1gy0@gmail.com>
2025-08-01 15:30:27 +0900

    add function

--- server.py
+++ server.py
@@ -1,14 +1,28 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE']
+
+@app.route('/', defaults={'path': ''}, methods = METHODS)
+@app.route('/<path:path>', methods=METHODS)
+def index(path):
     ua = request.headers.get('User-Agent', '')
     print('UA: ', ua)
     if "ChatGPT" in ua:
-        return "Hello, ChatGPT!"
-    return "Hello, Scanner!"
+        return "Access Denied", 403
+    if "virustotalcloud" in ua:
+        return "Access Denied", 403
+    return f'''
+    <h1>Path: /{path}</h1>
+    <h2>Method: {request.method}</h2>
+    <h2>Request headers</h2>
+    <ul>
+    {''.join((f'<li>{name}: {value}</li>' for name, value in request.headers))}
+    </ul>
+    <h2>Request body</h2>
+    <p>{request.get_data().decode() if request.get_data() else 'None.'}</p>
+    '''
 
 if __name__ == '__main__':
     app.run(port=8000)
